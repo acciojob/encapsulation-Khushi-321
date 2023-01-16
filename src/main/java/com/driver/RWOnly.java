be29496e06a83b7cc01b706ad01d5ef9ffc8b837
Khushi-321 <113753057+Khushi-321@users.noreply.github.com>
2023-01-16 18:23:11 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String x;
+    public String getName(){
+        return x;
+    }
+    public void setName(String  x){
+        this.x=x;
+    }
 }
